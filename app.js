959f94c2a710a358ee0773457004f41a86d7b836
Kellphy <virtual_44@yahoo.com>
2022-10-12 01:09:32 +0300

    check page full load

--- app.js
+++ app.js
@@ -121,7 +121,7 @@ async function viewRandomPage(browser, page) {
         console.log('\n🔗 Now watching streamer: ', baseUrl + watch);
 
         await page.goto(baseUrl + watch, {
-          "waitUntil": "load"
+          "waitUntil": "domcontentloaded"
         }); //https://github.com/puppeteer/puppeteer/blob/master/docs/api.md#pagegobackoptions
         console.log('✅ Stream loaded!');
         await clickWhenExist(page, cookiePolicyQuery);
