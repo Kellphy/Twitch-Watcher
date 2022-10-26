0a52e938e0c84c9189bc0d5de19e2d11f4491d55
Kellphy <virtual_44@yahoo.com>
2022-10-27 01:01:01 +0300

    waitfor upgrade pt 1

--- app.js
+++ app.js
@@ -129,7 +129,7 @@ async function viewRandomPage(browser, page) {
 
         //Check if the stream video player is loaded
         console.log('🔧 Waiting for Stream Settings Query..');
-        await page.waitForXPath(streamSettingsQuery);
+        await page.waitForSelector(streamSettingsQuery);
         console.log('🔧 Done.');
 
         if (firstRun) {
