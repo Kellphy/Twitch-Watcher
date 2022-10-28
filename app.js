b1548fb16dab9e578da2af6d197ea88cf2f06137
Kellphy <virtual_44@yahoo.com>
2022-10-28 17:54:45 +0300

    restore skippng

--- app.js
+++ app.js
@@ -147,7 +147,7 @@ async function viewRandomPage(browser, page) {
 
         if (firstRun) {
 
-          if(true){ //skipping for now
+          if(false){ //skipping for now
             console.log('🔧 Setting lowest possible resolution..');
             await clickWhenExist(page, streamPauseQuery);
             await takeScreenShot(page,watch);
